cd49a9b6ed3a14498c723aad861c52e2cbb26165
drakosa13 <dm@madfox.io>
2021-01-21 03:18:42 +0300

    one more day

--- src/processor/pointer.c
+++ src/processor/pointer.c
@@ -1,49 +1,42 @@
 
 #include "../include/ft_printf.h"
 
-static void num_to_hex(unsigned long long num)
+int base_converter(unsigned long long num, int notation)
 {
+    const char base[] = {
+        '0','1','2','3','4','5','6','7','8','9',
+        'A','B','C','D','E','F' };
+    int mas[64];
+    int i = 0;
 	char *string;
-	char *preffix;
+	char *p_sring;
 
-	ft_putnbr(num);
+    while (num != 0)
+    {
+        mas[i] = num % notation;
+        num = num / notation;
+		i++;
+    }
+	string = malloc(sizeof(char) * i + 1);
+	string[i] = '\0';
+	p_sring = string;
+    while(i-- > 0)
+		*p_sring++ = base[mas[i]];
+	p_sring = string;
+	string = ft_strjoin("0x", string);
+	free(p_sring);
+	ft_putstr(string);
 
-	preffix = ft_strdup("0x");
-	string = malloc(sizeof(char) * 12);
-
-	char		*ft_hex(char *hex, unsigned long int h, int razr, char reg)
-	{
-		int		o;
-
-		hex[razr--] = '\0';
-		while (razr >= 0)
-		{
-			o = h % 16;
-			if (h > 16 && o > 9)
-				hex[razr] = reg + (o - 9);
-			else if (h > 16 && o <= 9)
-				hex[razr] = o + '0';
-			else if (h < 16 && h > 9)
-				hex[razr] = reg + (h - 9);
-			else if (h <= 9)
-				hex[razr] = h + '0';
-			else
-				hex[razr] = '0';
-			razr--;
-			h = h / 16;
-		}
-		return (hex);
-	}
-
-
+    return 0;
 }
+
 
 void	draw_pointer(struct s_flags *flag, va_list *ap)
 {
 	unsigned long long num;
 	print_flags(flag);
 	num = va_arg(*ap, unsigned long long);
-	num_to_hex(num);
-	printf("\nnum: %llu\n", num);
+	base_converter(num, 16);
+//	printf("\nnum: %llu\n", num);
 
 }
